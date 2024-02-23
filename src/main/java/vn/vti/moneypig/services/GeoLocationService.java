2cda069f27d1a69ad90eec9272fecd7d9d73d0e2
dinhvandong <dongvan.ict3@gmail.com>
2024-02-23 21:20:38 +0700

    xxxFFFF
    dasdsadasd

--- src/main/java/vn/vti/moneypig/services/GeoLocationService.java
+++ src/main/java/vn/vti/moneypig/services/GeoLocationService.java
@@ -1,32 +1,32 @@
-package vn.vti.moneypig.services;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-import vn.vti.moneypig.dto.GeoLocationApiResult;
-import vn.vti.moneypig.dto.GeoLocationInfo;
-
-@Service
-public class GeoLocationService {
-
-    @Value("${geolocation.api.url}")
-    private String geolocationApiUrl;
-
-    public GeoLocationInfo getGeoLocationInfo(String ipAddress) {
-        RestTemplate restTemplate = new RestTemplate();
-        String apiUrl = geolocationApiUrl + ipAddress;
-
-        GeoLocationApiResult result = restTemplate.getForObject(apiUrl, GeoLocationApiResult.class);
-
-        if (result != null) {
-            String country = result.getCountry();
-            String city = result.getCity();
-            String latitude = result.getLatitude();
-            String longitude = result.getLongitude();
-
-            return new GeoLocationInfo(country, city, latitude, longitude);
-        }
-
-        return null;
-    }
-}
+//package vn.vti.moneypig.services;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.stereotype.Service;
+//import org.springframework.web.client.RestTemplate;
+//import vn.vti.moneypig.dto.GeoLocationApiResult;
+//import vn.vti.moneypig.dto.GeoLocationInfo;
+//
+//@Service
+//public class GeoLocationService {
+//
+//    @Value("${geolocation.api.url}")
+//    private String geolocationApiUrl;
+//
+//    public GeoLocationInfo getGeoLocationInfo(String ipAddress) {
+//        RestTemplate restTemplate = new RestTemplate();
+//        String apiUrl = geolocationApiUrl + ipAddress;
+//
+//        GeoLocationApiResult result = restTemplate.getForObject(apiUrl, GeoLocationApiResult.class);
+//
+//        if (result != null) {
+//            String country = result.getCountry();
+//            String city = result.getCity();
+//            String latitude = result.getLatitude();
+//            String longitude = result.getLongitude();
+//
+//            return new GeoLocationInfo(country, city, latitude, longitude);
+//        }
+//
+//        return null;
+//    }
+//}
