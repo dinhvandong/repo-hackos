666ec38fda30cb30ecc8fd9293b9921b05711264
dinhvandong <dongvan.ict3@gmail.com>
2024-04-24 00:15:21 +0700

    xxx
    xxxx

--- src/main/java/vn/vti/moneypig/utils/DateUtils.java
+++ src/main/java/vn/vti/moneypig/utils/DateUtils.java
@@ -1,6 +1,7 @@
 package vn.vti.moneypig.utils;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class DateUtils {
@@ -12,10 +13,17 @@ public class DateUtils {
         return Long.parseLong(formattedDate);
     }
 
-    public static long getCurrentDateYYYYMMDDHHmmss(){
-        LocalDate currentDate = LocalDate.now();
+//    public static long getCurrentDateYYYYMMDDHHmmss(){
+//        LocalDate currentDate = LocalDate.now();
+//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
+//        String formattedDate = currentDate.format(formatter);
+//        return Long.parseLong(formattedDate);
+//    }
+
+    public static long getCurrentDateYYYYMMDDHHmmss() {
+        LocalDateTime currentDateTime = LocalDateTime.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
-        String formattedDate = currentDate.format(formatter);
-        return Long.parseLong(formattedDate);
+        String formattedDateTime = currentDateTime.format(formatter);
+        return Long.parseLong(formattedDateTime);
     }
 }
